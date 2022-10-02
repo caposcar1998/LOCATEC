b596afc0073753a2f285c5762e37088d7ebb3276
Mauville <4991127@outlook.com>
2022-10-01 21:35:06 -0500

    Add Auth0 Integration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {Auth0Provider} from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>
+    <Auth0Provider
+        domain={"dev-2u-5y9fz.us.auth0.com"}
+        clientId={"8St2S8kY30UAh2iSsWbFSYjbEV9Uj3vg"}
+        redirectUri={window.location.origin}>
+        <App/>
+    </Auth0Provider>
+</React.StrictMode>)
