4b65a6634cd5812196a6b6be48b414d6ca17b68a
caposcar1998 <oscarcontrerascap@gmail.com>
2022-10-02 22:59:50 -0500

    added navBar and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,182 +1,26 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { useNavigate } from 'react-router-dom';
-import { Button, 
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  InputGroup,
-  Input,
-  Card,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  CardText
-} from 'reactstrap'
+import Catalogo from './Catalogo'
+import Header from './components/Header'
+import NoPage from './components/NoPage'
+import { Home } from './Home'
+
 
 
 
 function App() {
 
-  const [collapsed, setCollapsed] = useState(true);
-
-  const toggleNavbar = () => setCollapsed(!collapsed);
-
-  const navigate = useNavigate;
-
 
   return (
-    <div className="App">
-      <div>
-      <Navbar>
-          <NavbarBrand href="/" className='me-2'>
-            <img 
-              alt="logoi"
-              src="/logotec.png"
-              className='logoi'
-              style={{
-                height: 40,
-                width: 40,
-              }}
-            />
-            LOCATEC
-          </NavbarBrand>
-          <NavbarToggler onClick={toggleNavbar} className="me-2" />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
-            <NavItem>
-              <NavLink href="/components/">LogIn</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://youtube.com">
-                Catalogo
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-        </Navbar>
-      </div>
-      <div>
-        <a>
-          <img src="/campus.jpg" className="logoprueba" alt="Vite logo" style={{
-                height: 580,
-                width: 1020,
-              }} />
-        </a>
-      </div>
-      <div>
-        <a href="https://tec.mx/es" target="_blank">
-          <img src="/logoborrego.png" className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>¿Qué artículo estás buscando?</h1>
-      <div className='busqueda'>
-      <img src="/teus.png" className="logo" alt="teus logo" />
-      <InputGroup className='barra'>
-        <Input placeholder='El artículo que busco es...'/>
-        <Button color="primary">
-          Buscar
-        </Button>
-      </InputGroup>
-      </div>
-      <br />
-      <div>
-      <Button outline color="info" href="/Catalogo"> Ir al catálogo </Button>
-      </div>
-      <br />
-
-      <div className="cardi">
-        <Card
-          style={{
-            width: '18rem'
-          }}
-        >
-          <img
-            alt="Sample"
-            src="/raquetas.png"
-          />
-          <CardBody>
-            <CardTitle tag="h5">
-              Card title
-            </CardTitle>
-            <CardSubtitle
-              className="mb-2 text-muted"
-              tag="h6"
-            >
-              Card subtitle
-            </CardSubtitle>
-            <CardText>
-              Some quick example text to build on the card title and make up the bulk of the card‘s content.
-            </CardText>
-            <Button outline color="info">
-              Button
-            </Button>
-          </CardBody>
-        </Card>
-        <Card
-          style={{
-            width: '18rem'
-          }}
-        >
-          <img
-            alt="Sample"
-            src="/iphone.png"
-          />
-          <CardBody>
-            <CardTitle tag="h5">
-              Card title
-            </CardTitle>
-            <CardSubtitle
-              className="mb-2 text-muted"
-              tag="h6"
-            >
-              Card subtitle
-            </CardSubtitle>
-            <CardText>
-              Some quick example text to build on the card title and make up the bulk of the card‘s content.
-            </CardText>
-            <Button outline color="info">
-              Button
-            </Button>
-          </CardBody>
-        </Card>
-        <Card
-          style={{
-            width: '18rem'
-          }}
-        >
-          <img
-            alt="Sample"
-            src="/airpods.png"
-          />
-          <CardBody>
-            <CardTitle tag="h5">
-              Card title
-            </CardTitle>
-            <CardSubtitle
-              className="mb-2 text-muted"
-              tag="h6"
-            >
-              Card subtitle
-            </CardSubtitle>
-            <CardText>
-              Some quick example text to build on the card title and make up the bulk of the card‘s content.
-            </CardText>
-            <Button outline color="info">
-              Button
-            </Button>
-          </CardBody>
-        </Card>
-      </div>
-      <br/>
-      <p className="read-the-docs">
-        Calle del Puente 222, Col. Ejidos de Huipulco, Tlalpan, Ciudad de México, C.P. 14380 D.R.© INSTITUTO TECNOLÓGICO Y DE ESTUDIOS SUPERIORES DE MONTERREY, MÉXICO. 2022
-      </p>
-    </div>
+    <>
+      <Header/>
+        <Routes>
+            <Route exact path="/" element={<Home />}/>
+            <Route exact path="catalogo" element={<Catalogo />} />
+            <Route path="*" element={<NoPage />} />
+        </Routes>
+    </>
   )
 }
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+  } from 'reactstrap'
+  import '../App.css'
 
 const Header = () => {
+
+    const [collapsed, setCollapsed] = useState(true);
+
+    const toggleNavbar = () => setCollapsed(!collapsed);
+
     return(
-        <h1>Hola</h1>
+        <Navbar>
+        <NavbarBrand href="/" className='me-2'>
+          <img 
+            alt="logoi"
+            src="/logotec.png"
+            className='logoi'
+            style={{
+              height: 40,
+              width: 40,
+            }}
+          />
+          LOCATEC
+        </NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="me-2" />
+      <Collapse isOpen={!collapsed} navbar>
+        <Nav navbar>
+          <NavItem>
+            <NavLink href="/components/">LogIn</NavLink>
+          </NavItem>
+          <NavItem>
+            <Link to="/catalogo">Catalogo</Link>
+          </NavItem>
+        </Nav>
+      </Collapse>
+      </Navbar>
     );
 }
 
