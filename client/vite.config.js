41134c388c65fae29382ef9cc60b3e49ca3930fb
caposcar1998 <oscarcontrerascap@gmail.com>
2022-10-23 15:58:55 -0500

    added https to front

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true,
-    port: 8080
+    port: 8080,
+    https: true
   },
-  plugins: [react()]
+  plugins: [react(), mkcert()]
 })
