d2f3d7abcb094c32a47d2e2671a242d7eb695114
caposcar1998 <oscarcontrerascap@gmail.com>
2022-10-31 23:03:28 -0600

    added correct auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Catalogo from './Catalogo'
-import CreateProduct from './CreateProduct'
-import NoPage from './components/NoPage'
-import { Home } from './Home'
-
-
-
+import NoPage  from '../src/components/NoPage'
+import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
 
 function App() {
 
 
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
+  useAuth0();
+
   return (
-    <>
-      
-        <Routes>
-            <Route exact path="/" element={<Home />}/>
-            <Route exact path="catalogo" element={<Catalogo />} />
-            <Route exact path="createProduct" element={<CreateProduct />} />
-            <Route path="*" element={<NoPage />} />
-        </Routes>
-    </>
+    <Routes>
+      <Route exact path="/" element={
+        isAuthenticated ?
+        <button onClick={() => logout({ returnTo: window.location.origin })}></button>
+        :
+        <button onClick={loginWithRedirect}>Log in</button>
+      }/>
+      {user ? 
+        <>
+              <Route exact path='*' element={<NoPage/>}/> 
+            </> 
+      :
+          <Route exact path='/algo' element={<NoPage/>}/>
+      }
+
+    </Routes>
   )
 }
 
