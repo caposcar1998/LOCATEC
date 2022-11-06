1234a0cfc3bcf34a26e0c5b0cf8e59b9581bf2d6
caposcar1998 <oscarcontrerascap@gmail.com>
2022-11-06 16:10:02 -0600

    Added correct text

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,8 +5,6 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { FaBeer, FaFacebookSquare, FaTwitterSquare } from 'react-icons/fa';
-import Upload from './Helpers/Upload';
 
 function Home(){
 
@@ -24,9 +22,9 @@ function Home(){
             <br/>
             <Col>
             <Row className="justify-content-md-center">
-            <h1 class="tituloBlanco">Support Center & Knowledge base</h1>
+            <h1 class="tituloBlanco">LOCATEC</h1>
             <br/>
-            <h2 class="tituloBlanco">Find advice and answers from our support team fast or get in touch</h2>
+            <h2 class="tituloBlanco">Busca y encuentra tus productos perdidos con nosotros</h2>
             </Row>
             <br/>
              <Row className="justify-content-md-center">
@@ -44,17 +42,16 @@ function Home(){
             <br/>
 
             <div class="prueba1">
-                <h1>Find your answer by subject</h1>
+                <h1>Enciende el Cambio</h1>
                 <br></br>
                 <div class="presentacionCartas">
                     <div class="carta">
                     <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Img variant="top" src="https://serv1.raiolanetworks.es/blog/wp-content/uploads/seguridadwordpress1-300x300.png" />
                         <Card.Body>
-                        <Card.Title>Installation</Card.Title>
+                        <Card.Title>Seguridad</Card.Title>
                         <Card.Text>
-                            Lorem ipsum dolor sit amet ipsum
-                            dolor sit amet ipsum solor sit amet.
+                            Tus objetos perdidos siempre estaran seguros con nosotros
                         </Card.Text>
                         <Button variant="primary">Ir</Button>
                         </Card.Body>
@@ -62,12 +59,11 @@ function Home(){
                     </div>
                     <div class="carta">
                     <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/2103/2103853.png" />
                         <Card.Body>
-                        <Card.Title>Billing & Pricing</Card.Title>
+                        <Card.Title>Búsqueda eficiente</Card.Title>
                         <Card.Text>
-                            Lorem ipsum dolor sit amet ipsum
-                            dolor sit amet ipsum solor sit amet.
+                            Encuentra tus productos a unos clicks de distancia
                         </Card.Text>
                         <Button variant="primary">Ir</Button>
                         </Card.Body>
@@ -75,12 +71,11 @@ function Home(){
                     </div>
                     <div class="carta">
                     <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/6116/6116634.png" />
                         <Card.Body>
-                        <Card.Title>Features</Card.Title>
+                        <Card.Title>Alta disponibilidad</Card.Title>
                         <Card.Text>
-                            Lorem ipsum dolor sit amet ipsum
-                            dolor sit amet ipsum solor sit amet.
+                            Busca en cualquier momento tu producto perdido
                         </Card.Text>
                         <Button variant="primary">Ir</Button>
                         </Card.Body>
@@ -92,9 +87,8 @@ function Home(){
             <br/>
             <br />
             <div class="cuadro">
-                <h1>Didn’t find an answer to your question?</h1>
-                <p>Musce libero nunc, dignissim quis turpis quis, semper vehicula dolor. Suspendisse tincidunt consequat quam.</p>
-            <Button>Contact us</Button>
+                <h1>Búsqueda rápida</h1>
+                <p></p>
            </div>
 
            <br/>
@@ -103,10 +97,9 @@ function Home(){
            <footer>
            <div class="pie">
             <div>
-            <p>Copyright © 2020 Designed and Developed by themefisher.</p>
+            <p>Técnologico de Monterrey, LOCATEC por: Oscar Contreras, Oscar Belmont, David Arturo y Oscar Vargas</p>
             </div>
             <div>
-            <p><FaFacebookSquare/><FaTwitterSquare/></p>
             </div>
             </div>
            </footer>
