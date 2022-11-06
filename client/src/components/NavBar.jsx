25f61d0f64e2b5357dcffee00ffacfd8cfb5c184
caposcar1998 <oscarcontrerascap@gmail.com>
2022-11-05 19:08:15 -0600

    change navbar option

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -30,11 +30,13 @@ const NavBar = () =>{
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link onClick={() => {handleClick("/");}}>Home</Nav.Link>
-            <Nav.Link onClick={() => {handleClick("/catalogo");}}>Objetos</Nav.Link>
-            <Nav.Link onClick={() => {handleClick("/usuario");}}>Usuarios</Nav.Link>
             {
                 isAuthenticated ?
+                  <>
+                    <Nav.Link onClick={() => {handleClick("/catalogo");}}>Objetos</Nav.Link>
+                    <Nav.Link onClick={() => {handleClick("/usuario");}}>Usuarios</Nav.Link>
                     <Nav.Item><Button variant="outline-danger" onClick={LogOut}>Logout</Button></Nav.Item> 
+                  </>
                 :
                     <Nav.Item><Button variant="outline-success" onClick={loginWithRedirect}>Login</Button></Nav.Item>
             }
